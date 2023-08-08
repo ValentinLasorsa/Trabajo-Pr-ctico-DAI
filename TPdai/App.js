04a5fadd1b6f74370c32ce35cdb6073a2161de70
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 08:58:43 -0300

    2do commit

--- TPdai/App.js
+++ TPdai/App.js
@@ -1,35 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, SafeAreaView, Button, TouchableOpacity, Pressable} from 'react-native';
-
+import { StyleSheet, Text, TextInput, SafeAreaView, Button, Alert, TouchableOpacity, Pressable, Keyboard} from 'react-native';
+import React, { PureComponent } from "react";
 const App = () => {  
   const [name, handleNombreChange] = React.useState('');  
   const [cel, onChangeNumber] = React.useState('');
+  const [mail, onChangeMail] = React.useState('');
+  const [clave, onChangeClave] = React.useState('');
   return (    
   <SafeAreaView style={styles.container}>      
-  <Text>Nombre</Text>      
+  <StatusBar style='auto'></StatusBar>
+  <Text  style={styles.text}>Nombre</Text>      
   <TextInput        
-  style={styles.input}        
-  placeholder="ingrese su nombre"        
+  style={styles.input}              
   onChangeText={handleNombreChange}        
   value={name}
   />
-  <Text>Teléfono</Text>      
+  <Text style={styles.text}>Teléfono</Text>      
   <TextInput
         style={styles.input}
         onChangeText={onChangeNumber}
         value={cel}
-        placeholder="ingrese su teléfono"
         keyboardType="numeric"
       />
+      <Text style={styles.text}>Email</Text>      
+  <TextInput
+        style={styles.input}
+        onChangeText={onChangeMail}
+        value={mail}
+      />
+      <Text style={styles.text}>Clave</Text>      
+  <TextInput
+        secureTextEntry={true}
+        style={styles.input}
+        onChangeText={onChangeClave}
+        value={clave}
+      />
+      <Button
+      title="Aceptar (button)"
+      onPress={() => console.log("Nombre:", {name}," Teléfono:", {cel}," Email:",{mail}," Clave:",{clave})}
+      ></Button>
+      
   </SafeAreaView>  
   ); 
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    fontSize:48,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+
+
+export default App;
+
