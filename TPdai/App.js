899ad32e18bcd7918c09679d0b29dc606211e783
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-11 09:27:55 -0300

    falta keyboard

--- TPdai/App.js
+++ TPdai/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, SafeAreaView, Button, Alert, TouchableOpacity, Pressable, Keyboard} from 'react-native';
+import { StyleSheet, Text, TextInput, SafeAreaView, Button, Alert, TouchableOpacity, Pressable, Keyboard,KeyboardAvoidingView , ImageBackground} from 'react-native';
 import React, { PureComponent } from "react";
 const App = () => {  
   const [name, handleNombreChange] = React.useState('');  
@@ -7,8 +7,14 @@ const App = () => {
   const [mail, onChangeMail] = React.useState('');
   const [clave, onChangeClave] = React.useState('');
   return (    
+    <KeyboardAvoidingView >
   <SafeAreaView style={styles.container}>      
+<ImageBackground 
+  source={require('./assets/sunny.png')} 
+  style={styles.backgroundImage} >
+        
   <StatusBar style='auto'></StatusBar>
+  
   <Text  style={styles.text}>Nombre</Text>      
   <TextInput        
   style={styles.input}              
@@ -35,28 +41,88 @@ const App = () => {
         onChangeText={onChangeClave}
         value={clave}
       />
+      
       <Button
+      style={styles.button}
       title="Aceptar (button)"
       onPress={() => console.log("Nombre:", {name}," Teléfono:", {cel}," Email:",{mail}," Clave:",{clave})}
-      ></Button>
+      >Aceptar (button)</Button>
       <TouchableOpacity
+       style={styles.touchable}
       title="Aceptar (Touchable)"
       onPress={() => console.log("Nombre:", {name}," Teléfono:", {cel}," Email:",{mail}," Clave:",{clave})}
-      ><Text>Aceptar (Touchable)</Text></TouchableOpacity>
+      ><Text style={styles.text1}>Aceptar (Touchable)</Text>
+      </TouchableOpacity>
+      <Pressable 
+      style={styles.pressable}
+      onPress={() => console.log("Nombre:",{name}, " Teléfono", {cel}," Email:",{mail}," Clave:",{clave})}>  
+      <Text style={styles.text1}>Aceptar (Pressable)</Text> 
+      </Pressable>
+      </ImageBackground>
   </SafeAreaView>  
+  </KeyboardAvoidingView >
   ); 
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize:48,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: "white",
+  },
+  text1:{
+    fontSize:15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: "white",
   },
   input: {
     height: 40,
     margin: 12,
-    borderWidth: 1,
+    borderWidth: 3,
     padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  button:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    width: "100%",
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue',
+  },
+  touchable:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    width: "100%",
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'yellow',  
+  },
+  pressable:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    width: "100%",
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'dodgerblue',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: 1920,
+    height: 1080,
+    flexDirection: 'column',
+    backgroundColor:'transparent',
+    justifyContent: 'flex-start',
+},
 });
 
 
